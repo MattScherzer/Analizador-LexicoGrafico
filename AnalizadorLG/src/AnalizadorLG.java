5d2bef7298e4d794740b71014977862057fee28c
MattScherzer <fritzmattheas@gmail.com>
2013-12-03 03:04:03 -0600

    Guardar

---
+++ AnalizadorLG/src/AnalizadorLG.java
@@ -0,0 +1,237 @@
+/**
+ * @(#)AnalizadorLG.java
+ *
+ * Analizador Lexico Grafico
+ * Proyecto de Teoria de la computacion
+ * CUCEI 2013B
+ *
+ * @author Matt Scherzer
+ * @version 1.00 2013/11/27
+ */
+ 
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+import javax.swing.*; 
+ 
+public class AnalizadorLG extends JFrame implements ActionListener
+{
+	
+	JTextField txtInsertar, txtResultado, txtEstado;
+	JButton btnAnalizar, btnBorrar, btnSalir;
+	JLabel lblAnalizar,lblEstado, lblResultado;
+	
+	String strEstados = "0"; // cadena que imprime los estados por los que pasa la cadena al ser validada
+	String strResultado = ""; // cadena que imprime el resultado de la cadena despues de ser validada
+	
+	boolean signo = false, punto = false, expresion_artimetica = false, reservada = false;
+	
+	AnalizarCasos analisis = new AnalizarCasos(); // inicializar clase
+	
+	public AnalizadorLG()
+	{
+		Visual();
+	}
+	
+	public void Visual()
+	{
+		super.setTitle("Analizador Léxico-Gráfico");
+		this.setBounds(500,170,350,350);
+    	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		
+		// LABEL
+		lblAnalizar = new JLabel("INTRODUCE UNA CADENA:");
+		lblAnalizar.setBounds(95,25,150,25);
+		lblEstado = new JLabel("ESTADOS:");
+		lblEstado.setBounds(145,95,150,25);
+		lblResultado = new JLabel("RESULTADO:");
+		lblResultado.setBounds(140,175,150,25);
+		// TEXT FIELD
+		txtInsertar = new JTextField();
+		txtInsertar.setBounds(20,55,310,25);
+		txtEstado = new JTextField();
+		txtEstado.setBounds(20,120,310,25);
+		txtResultado = new JTextField();
+		txtResultado.setBounds(20,200,310,25);
+		// BUTTONS
+		btnAnalizar = new JButton("Analizar");
+		btnAnalizar.setBounds(20,260,100,25);
+		btnBorrar = new JButton("Borrar");
+		btnBorrar.setBounds(125,260,100,25);
+		btnSalir = new JButton("Salir");
+		btnSalir.setBounds(230,260,100,25);	
+
+		setLayout(null);
+		
+		// ADD LABEL
+		this.add(lblAnalizar);
+		this.add(lblEstado);
+		this.add(lblResultado);
+		// ADD TEXT FIELD
+		this.add(txtInsertar);		
+		this.add(txtEstado);
+		this.add(txtResultado);
+		// ADD BUTTONS
+		this.add(btnAnalizar);
+		this.add(btnBorrar);
+		this.add(btnSalir);
+
+		// VISIBLE
+		this.setVisible(true);
+
+		// EDITABLE
+		txtEstado.setEditable(false);
+		txtResultado.setEditable(false);
+
+		// ACTION LISTENER
+		btnSalir.addActionListener(this);
+		btnBorrar.addActionListener(this);
+	    btnAnalizar.addActionListener(this);
+	}
+	
+	public void actionPerformed(ActionEvent evt)
+	{
+		Object click=evt.getSource();
+		
+		if(btnAnalizar == click)
+		{
+			String cadena = txtInsertar.getText();
+			Analizar(cadena);
+		}
+		if(btnBorrar == click)
+		{
+			txtInsertar.setText("");
+			txtEstado.setText("");
+			txtResultado.setText("");
+			strEstados = "0";
+		}
+		if(btnSalir == click)
+		{
+			System.exit(0);
+		}
+	} 
+	
+	public void Analizar(String cadena)
+	{
+		int i=0, estado=0, caso=0; //inicializar el validador de estados y el contador
+		int tamanio = cadena.length(); // recibir longitud de cadena
+
+		do
+		{
+			caso = estado;
+			estado = analisis.Validar(cadena.charAt(i), caso, cadena);
+			
+			switch(estado)
+			{
+				case 1:
+					strEstados = strEstados + ",1";
+					strResultado = "IDENTIFICADOR";
+				break;
+				case 2:
+					strEstados = strEstados + ",2";
+					strResultado = "ENTERO";
+				break;
+				case 3:
+					strEstados = strEstados + ",3";
+					strResultado = "ENTERO";
+				break;
+				case 4:
+					strEstados = strEstados + ",4";
+					strResultado = "ENTERO";
+				break;
+				case 5:
+					strEstados = strEstados + ",5";
+					strResultado = "ENTERO";
+				break;
+				case 6:
+					strEstados = strEstados + ",6";
+					if(expresion_artimetica == true)
+					{
+						strResultado = "EXPRESIÓN ARITMETICA";
+					}
+					else if(signo == true)
+					{
+						strResultado = "ENTERO CON SIGNO";
+					}
+					else
+					{
+						strResultado = "ENTERO";
+					}
+				break;
+				case 7:
+					strEstados = strEstados + ",7";
+				break;
+				case 8:
+					strEstados = strEstados + ",8";
+				break;
+				case 9:
+					strEstados = strEstados + ",9";
+				break;
+				case 10:
+					strEstados = strEstados + ",10";
+					strResultado = "HEXADECIMAL";
+				break;
+				case 11:
+					strEstados = strEstados + ",11";
+					signo = true;
+				break;
+				case 12:
+					strEstados = strEstados + ",12";
+					punto = true;
+				break;
+				case 13:
+					strEstados = strEstados + ",13";
+					expresion_artimetica = true;
+				break;
+				case 14:
+					strEstados = strEstados + ",14";
+					strResultado = "EXPONENCIAL";
+				break;
+				case 15:
+					strEstados = strEstados + ",15";
+					strResultado = "PALABRA RESERVADA";
+					reservada = true;
+				break;
+				case 16:
+					strEstados = strEstados + ",16";
+					if(signo==true)
+					{
+						strResultado = "REAL CON SIGNO";
+					}
+					else
+					{
+						strResultado = "REAL";
+					}
+				break;
+				case 17:
+					strEstados = strEstados + ",17";
+				break;
+				case 18:
+					strEstados = strEstados + ",18";
+					strResultado = "EXPONENCIAL";
+				case 19: default:
+					strResultado = "ERROR";
+				break;
+			}
+			if(reservada == true)
+			{
+				break;
+			}
+			
+			i++;
+			
+		}while(i < tamanio);
+		
+		txtEstado.setText(strEstados);
+		txtResultado.setText(strResultado);
+	}
+	
+    public static void main(String[] args)
+    {
+    	AnalizadorLG alg = new AnalizadorLG();
+    }
+}
---
+++ AnalizadorLG/src/AnalizarCasos.java
@@ -0,0 +1,331 @@
+/**
+ * @(#)AnalizarCasos.java
+ *
+ * Analizador Lexico Grafico
+ * Proyecto de Teoria de la computacion
+ * CUCEI 2013B
+ *
+ * @author Matt Scherzer
+ * @version 1.00 2013/11/27
+ */
+ 
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+public class AnalizarCasos
+{
+	public int Validar(char caracter, int caso, String cadena)
+	{		
+		switch(caso) // validar lo que puede pasar en los distintos casos
+		{
+			case 0:
+				if(Character.isLetter(caracter) == true)
+				{
+					if(PalabraReservada(cadena) == true)
+					{
+						return 15;
+					}
+					else
+					{
+						return 19;
+					}
+				}
+				else if(Character.isLetter(caracter) == true || caracter == '_')
+				{
+					return 1;
+				}
+				else if(Character.isDigit(caracter) == true)
+				{
+					return 2; 
+				}
+				else if(caracter == '+' || caracter == '-')
+				{
+					return 11;
+				}
+				else
+				{
+					return 19;
+				}
+			case 1:
+				// El caso uno es recursivo y puede mandar a caso de error
+				if(Character.isLetter(caracter) == true || Character.isDigit(caracter) == true || caracter == '_')
+				{
+					return 1; // se hace recursivo hasta el fin de cadena
+				}
+				else
+				{
+					return 19; // caso de error en caso de leer un caracter no valido
+				}
+			case 2:
+				// El caso dos puede mandar a 5 casos diferentes + caso de error
+				if(Character.isDigit(caracter) == true)
+				{
+					return 3;
+				}
+				else if(caracter == 'A' || caracter == 'B' || caracter == 'C' || caracter == 'D' || caracter == 'E' || caracter == 'F')
+				{
+					return 7;
+				}
+				else if(caracter == '.')
+				{
+					return 12;
+				}
+				else if(caracter == '+' || caracter == '-' || caracter == '*' || caracter == '/' || caracter == '^')
+				{
+					return 13;
+				}
+				else if(caracter == 'e')
+				{
+					return 14;
+				}
+				else
+				{
+					return 19; // caso de error en caso de leer un caracter no valido
+				}
+			case 3:
+				// El caso tres puede mandar a 5 casos diferentes + caso de error
+				if(Character.isDigit(caracter) == true)
+				{
+					return 4;
+				}
+				else if(caracter == 'A' || caracter == 'B' || caracter == 'C' || caracter == 'D' || caracter == 'E' || caracter == 'F')
+				{
+					return 8;
+				}
+				else if(caracter == '.')
+				{
+					return 12;
+				}
+				else if(caracter == '+' || caracter == '-' || caracter == '*' || caracter == '/' || caracter == '^')
+				{
+					return 13;
+				}
+				else if(caracter == 'e')
+				{
+					return 14;
+				}
+				else
+				{
+					return 19; // caso de error en caso de leer un caracter no valido
+				}
+			case 4:
+				// El caso cuatro puede mandar a 5 casos diferentes + caso de error
+				if(Character.isDigit(caracter) == true)
+				{
+					return 5;
+				}
+				else if(caracter == 'A' || caracter == 'B' || caracter == 'C' || caracter == 'D' || caracter == 'E' || caracter == 'F')
+				{
+					return 9;
+				}
+				else if(caracter == '.')
+				{
+					return 12;
+				}
+				else if(caracter == '+' || caracter == '-' || caracter == '*' || caracter == '/' || caracter == '^')
+				{
+					return 13;
+				}
+				else if(caracter == 'e')
+				{
+					return 14;
+				}
+				else
+				{
+					return 19; // caso de error en caso de leer un caracter no valido
+				}
+			case 5:
+				// El caso cinco puede mandar a 5 casos diferentes + caso de error
+				if(Character.isDigit(caracter) == true)
+				{
+					return 6;
+				}
+				else if(caracter == 'H' || caracter == 'h')
+				{
+					return 10;
+				}
+				else if(caracter == '.')
+				{
+					return 12;
+				}
+				else if(caracter == '+' || caracter == '-' || caracter == '*' || caracter == '/' || caracter == '^')
+				{
+					return 13;
+				}
+				else if(caracter == 'e')
+				{
+					return 14;
+				}
+				else
+				{
+					return 19; // caso de error en caso de leer un caracter no valido
+				}
+			case 6:
+				// El caso seis es recursivo y puede mandar a 3 casos diferentes + caso de error
+				if(Character.isDigit(caracter) == true)
+				{
+					return 6;
+				}
+				else if(caracter == '.')
+				{
+					return 12;
+				}
+				else if(caracter == '+' || caracter == '-' || caracter == '*' || caracter == '/' || caracter == '^')
+				{
+					return 13;
+				}
+				else if(caracter == 'e')
+				{
+					return 14;
+				}
+				else
+				{
+					return 19; // caso de error en caso de leer un caracter no valido
+				}
+			case 7:
+				// El caso siete puede mandar a 1 caso diferente + caso de error
+				if(Character.isDigit(caracter) == true ||
+					caracter == 'A' || caracter == 'B' || caracter == 'C' || caracter == 'D' || caracter == 'E' || caracter == 'F')
+				{
+					return 8;
+				}
+				else
+				{
+					return 19;  // caso de error en caso de leer un caracter no valido
+				}
+			case 8:
+				// El caso ocho puede mandar a 1 caso diferente + caso de error
+				if(Character.isDigit(caracter) == true ||
+					caracter == 'A' || caracter == 'B' || caracter == 'C' || caracter == 'D' || caracter == 'E' || caracter == 'F')
+				{
+					return 9;
+				}
+				else
+				{
+					return 19;  // caso de error en caso de leer un caracter no valido
+				}				
+			case 9:
+				// El caso nueve puede mandar a 1 caso diferente + caso de error
+				if(caracter == 'H' || caracter == 'h')
+				{
+					return 10;
+				}
+				else
+				{
+					return 19;  // caso de error en caso de leer un caracter no valido
+				}
+			case 11: case 13:
+				// El caso once puede mandar a 1 caso diferente + caso de error
+				if(Character.isDigit(caracter) == true)
+				{
+					return 6;
+				}
+				else
+				{
+					return 19;  // caso de error en caso de leer un caracter no valido
+				}
+			case 12:
+				// El caso doce puede mandar a 1 caso diferente + caso de error
+				if(Character.isDigit(caracter) == true)
+				{
+					return 16;
+				}
+				else
+				{
+					return 19;  // caso de error en caso de leer un caracter no valido
+				}
+			case 14:
+				// El caso catorce puede mandar a 1 caso diferente + caso de error
+				if(caracter == '+' || caracter == '-')
+				{
+					return 17;
+				}
+				else
+				{
+					return 19; // si no entra en ninguno de los casos anteriores manda a un estado de error
+				}
+			case 16:
+				// El caso dieciseis es recursivo y puede mandar a 1 caso diferente + caso de error
+				if(Character.isDigit(caracter) == true)
+				{
+					return 16;
+				}
+				else if(caracter == '+' || caracter == '-' || caracter == '*' || caracter == '/' || caracter == '^')
+				{
+					return 13;
+				}
+				else
+				{
+					return 19; // si no entra en ninguno de los casos anteriores manda a un estado de error
+				}
+			case 17: case 18:
+				// El caso diecisiete puede mandar a 1 caso diferente + caso de error
+				if(Character.isDigit(caracter) == true)
+				{
+					return 18;
+				}
+				else
+				{
+					return 19; // si no entra en ninguno de los casos anteriores manda a un estado de error
+				}
+			case 10: default:
+				return 19;
+		}
+	}
+	
+	public boolean PalabraReservada(String palabra)
+    {
+    	boolean correcto = false;
+    	
+    	if(palabra.equalsIgnoreCase("int"))
+			correcto=true;
+			
+		if(palabra.equalsIgnoreCase("float"))
+			correcto=true;
+			
+		if(palabra.equalsIgnoreCase("double"))
+			correcto=true;
+			
+		if(palabra.equalsIgnoreCase("byte"))
+			correcto=true;
+			
+		if(palabra.equalsIgnoreCase("char"))
+			correcto=true;
+			
+		if(palabra.equalsIgnoreCase("boolean"))
+		   correcto=true;
+		   
+		if(palabra.equalsIgnoreCase("true"))
+			correcto=true;
+			
+		if(palabra.equalsIgnoreCase("false"))
+			correcto=true;
+			
+		if(palabra.equalsIgnoreCase("if"))
+			correcto=true;
+			
+		if(palabra.equalsIgnoreCase("else"))
+			correcto=true;
+			
+		if(palabra.equalsIgnoreCase("switch"))
+			correcto=true;
+			
+		if(palabra.equalsIgnoreCase("case"))
+			correcto=true;
+			
+		if(palabra.equalsIgnoreCase("default"))
+			correcto=true;
+			
+		if(palabra.equalsIgnoreCase("while"))
+			correcto=true;
+			
+		if(palabra.equalsIgnoreCase("for"))
+			correcto=true;
+			
+		return correcto;
+    }
+}
